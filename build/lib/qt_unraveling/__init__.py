2854201892d039df36e9344cb747acc0ded11e4c
Diego Veloza <dvelozad@unal.edu.co>
2021-06-02 17:20:29 -0500

    init corrected

--- build/lib/qt_unraveling/__init__.py
+++ build/lib/qt_unraveling/__init__.py
@@ -1,1 +1,1 @@
-from qt_unraveling.qt_unraveling import System, condition_check, representation, rhoBlochrep_data, fidelity, opCom, opAntiCom, opD, opH, opG, op_expect
+from qt_unraveling.qt_unraveling import System, condition_check, representation, rhoBlochrep_data, fidelity, opCom, opAntiCom, opD, opH, opG, op_expect,sigmax,sigmay,sigmaz,sigmap,sigmam,concurrence_psi,concurrence_rho
