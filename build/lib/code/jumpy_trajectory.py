ab82822e83d35c136470a2ad97eaa3a7e8ec63ef
Diego Veloza Diaz <dvelozad@unal.edu.co>
2022-11-27 15:50:06 -0500

    main code restructured around numba library

---
+++ build/lib/code/jumpy_trajectory.py
@@ -0,0 +1,135 @@
+'''
+***************************************
+Project: Quantum trajectory integrator
+Author: Diego Veloza Diaz
+Email: dvelozad@unal.edu.co
+Year: 2022
+***************************************
+'''
+import numpy as np
+from numba import njit, objmode, float64, complex128, int64
+import usual_operators_ as op
+import misc_func as misc
+
+@njit
+def dNRho(stateRho, measurement_op_list, dt, seed=0):
+    weight = np.zeros(np.shape(measurement_op_list)[0], dtype=np.float64)
+    M_index = np.zeros(np.shape(measurement_op_list)[0], dtype=np.int32)
+
+    for mu, Mmu in enumerate(measurement_op_list):
+        Mmu = np.ascontiguousarray(Mmu)
+        Mmu_dag_Mmu = np.dot(np.conjugate(np.transpose(Mmu)), Mmu)
+        weight[mu] += dt*np.real(np.trace(np.dot(stateRho, Mmu_dag_Mmu)))
+        M_index[mu] += mu
+    
+    np.random.seed(seed)
+    R = np.random.rand()
+    if R < sum(weight): 
+        jump_index = misc.numba_choice(M_index, weight, 1)[0]
+    else:
+        jump_index = np.shape(measurement_op_list)[0]
+    return jump_index
+
+@njit
+def ortogonal_mixing(oMatrix, coherent_fields, L_it):
+    new_ops = np.zeros(np.shape(L_it), dtype=np.complex128)
+    for n_O, O in enumerate(oMatrix):
+        new_ops[n_O] += coherent_fields[n_O]*np.eye(np.shape(L_it)[0])
+        for n_L, L in enumerate(L_it):
+            new_ops[n_O] += O[n_L]*L
+    return new_ops
+
+@njit
+def coherent_field_mixing(coherent_fields, L_it):
+    new_ops = np.zeros(np.shape(L_it), dtype=np.complex128)
+    for n_L, L in enumerate(L_it):
+        new_ops[n_L] += L + coherent_fields[n_L]*np.eye(np.shape(L_it)[0])
+    return new_ops
+
+@njit
+def jumpRhoTrajectory_td(initialStateRho, timelist, drivingH, original_lindbladList, eta_diag, lindbladList, coherent_fields, seed=0):
+    ## Timelist details
+    timeSteps = np.shape(timelist)[0]
+    dt = timelist[1] - timelist[0]
+
+    ## Number of Lindblad operators
+    num_lindblad_channels = np.shape(lindbladList(timelist[0],initialStateRho))[0]
+
+    rho_trajectory = np.ascontiguousarray(np.zeros(np.shape(timelist) + np.shape(initialStateRho), dtype=np.complex128))
+    rho_trajectory[0] += initialStateRho
+
+    no_jump_term_1 = np.ascontiguousarray(np.zeros(np.shape(initialStateRho), dtype=np.complex128))
+    no_jump_term_2 = np.ascontiguousarray(np.zeros(np.shape(initialStateRho), dtype=np.complex128))
+    inefficient_term = np.ascontiguousarray(np.zeros(np.shape(initialStateRho), dtype=np.complex128))
+    for n_it, it in enumerate(timelist[:-1]):
+        L_it = lindbladList(it, rho_trajectory[n_it])
+        original_L_it = original_lindbladList(it)
+
+        for n_i, L_i in enumerate(original_L_it):
+            inefficient_term += (1 - eta_diag[n_i])*op.D(L_i, rho_trajectory[n_it])*dt
+
+        ## coherent_field mix
+        #ortogonal_ops = ortogonal_mixing(oMatrix, coherent_fields, L_it)
+        coherent_field_ops = coherent_field_mixing(coherent_fields, L_it)
+
+        ## Jump index
+        jump_index = dNRho(rho_trajectory[n_it], coherent_field_ops, dt, seed*timeSteps+n_it)
+
+        ## Euler step
+        if jump_index < num_lindblad_channels:
+            rho_trajectory[n_it+1] += rho_trajectory[n_it] + op.G(coherent_field_ops[jump_index], rho_trajectory[n_it]) + inefficient_term
+
+        elif jump_index == num_lindblad_channels:
+            for n_r, L_r in enumerate(L_it):
+                L_r = np.ascontiguousarray(L_r)
+                no_jump_term_1 += -0.5*np.dot(np.transpose(np.conjugate(L_r)), L_r)
+                no_jump_term_2 += -coherent_fields[n_r]*L_r
+            
+            rho_trajectory[n_it+1] += rho_trajectory[n_it] + op.H(-1j*drivingH(it) + no_jump_term_1 + no_jump_term_2, rho_trajectory[n_it])*dt + inefficient_term
+            no_jump_term_1, no_jump_term_2, inefficient_term = 0*no_jump_term_1, 0*no_jump_term_2, 0*inefficient_term
+
+    return rho_trajectory
+
+@njit(complex128[:,:,:](complex128[:,:], float64[:], complex128[:,:], complex128[:,:,:], complex128[:], complex128[:,:,:], complex128[:], int64))
+def jumpRhoTrajectory_(initialStateRho, timelist, drivingH, original_lindbladList, eta_diag, lindbladList, coherent_fields, seed=0):
+    ## Timelist details
+    timeSteps = np.shape(timelist)[0]
+    dt = timelist[1] - timelist[0]
+
+    ## Number of Lindblad operators
+    num_lindblad_channels = np.shape(lindbladList)[0]
+
+    rho_trajectory = np.ascontiguousarray(np.zeros(np.shape(timelist) + np.shape(initialStateRho), dtype=np.complex128))
+    rho_trajectory[0] += initialStateRho
+
+    no_jump_term_1 = np.ascontiguousarray(np.zeros(np.shape(initialStateRho), dtype=np.complex128))
+    no_jump_term_2 = np.ascontiguousarray(np.zeros(np.shape(initialStateRho), dtype=np.complex128))
+    inefficient_term = np.ascontiguousarray(np.zeros(np.shape(initialStateRho), dtype=np.complex128))
+    for n_it, it in enumerate(timelist[:-1]):
+        L_it = lindbladList
+        original_L_it = original_lindbladList
+
+        for n_i, L_i in enumerate(original_L_it):
+            inefficient_term += (1 - eta_diag[n_i])*op.D(L_i, rho_trajectory[n_it])*dt
+
+        ## coherent_field mix
+        #ortogonal_ops = ortogonal_mixing(oMatrix, coherent_fields, L_it)
+        coherent_field_ops = coherent_field_mixing(coherent_fields, L_it)
+
+        ## Jump index
+        jump_index = dNRho(rho_trajectory[n_it], coherent_field_ops, dt, seed*timeSteps+n_it)
+
+        ## Euler step
+        if jump_index < num_lindblad_channels:
+            rho_trajectory[n_it+1] += rho_trajectory[n_it] + op.G(coherent_field_ops[jump_index], rho_trajectory[n_it]) + inefficient_term
+
+        elif jump_index == num_lindblad_channels:
+            for n_r, L_r in enumerate(L_it):
+                L_r = np.ascontiguousarray(L_r)
+                no_jump_term_1 += -0.5*np.dot(np.transpose(np.conjugate(L_r)), L_r)
+                no_jump_term_2 += -coherent_fields[n_r]*L_r
+            
+            rho_trajectory[n_it+1] += rho_trajectory[n_it] + op.H(-1j*drivingH + no_jump_term_1 + no_jump_term_2, rho_trajectory[n_it])*dt + inefficient_term
+            no_jump_term_1, no_jump_term_2, inefficient_term = 0*no_jump_term_1, 0*no_jump_term_2, 0*inefficient_term
+
+    return rho_trajectory
--- setup.py
+++ setup.py
@@ -3,30 +3,30 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
 setuptools.setup(
-  name = 'qt_unraveling',
-  packages = ['qt_unraveling'], 
-  version = '0.1.2',
-  license='MIT',
-  description = 'Library focused on simulate quantum trajectories with different unravelings',
-  author = 'Diego Veloza Diaz', 
-  author_email = 'dvelozad@unal.edu.co',
-  url = 'https://github.com/dvelozad',  
-  download_url = 'https://github.com/dvelozad/qt-unraveling-python/archive/refs/tags/v0.1.2.tar.gz',    
-  keywords = ['python','quantum control', 'unraveling', 'master equation', 'lindblad', 'open systems'],   
-  install_requires=[            # I get to this in a second
+    name="qt_unraveling",
+    version="0.2.0",
+    author="Diego Veloza Diaz",
+    author_email="dvelozad@unal.edu.co",
+    description="Library focused on simulate quantum trajectories with different unravelings",
+    url="https://github.com/dvelozad/qt-unraveling-python",
+    keywords = ['python','quantum control', 'unraveling', 'master equation', 'lindblad', 'open systems'],
+    download_url = 'https://github.com/dvelozad/qt-unraveling-python/archive/refs/tags/v0.2.0.tar.gz',   
+    packages=setuptools.find_packages(),
+    install_requires=[            # I get to this in a second
           'numpy',
-          'scipy',
+          'numba'
       ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      
-    'Intended Audience :: Science/Research',      
-    'Topic :: Scientific/Engineering :: Physics',
-    'License :: OSI Approved :: MIT License',   
-    'Programming Language :: Python :: 3',     
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',      
+        'Intended Audience :: Science/Research',      
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3',     
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
 )
